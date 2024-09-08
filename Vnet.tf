968ec4227d79c39e3feb822de2817e0876c8e5c6
Mario Lebrun <125732067+netmico@users.noreply.github.com>
2024-09-08 08:34:19 -0400

    Update Vnet.tf

--- Vnet.tf
+++ Vnet.tf
@@ -31,7 +31,7 @@ resource "azurerm_network_security_rule" "inbound_rule" {
     protocol                    = "TCP"
     source_port_range           = "*"
     destination_port_range      = "22"
-    source_address_prefix       = "*"
+    source_address_prefix       = "Your Local Public-IP"  
     destination_address_prefix  = "*"
     resource_group_name         = azurerm_resource_group.az_resources.name
     network_security_group_name = azure_network_security_group.adc_sec_gr.name
