bf6a9df87a618a0779bcbff13dcd120ef395b5cc
Mario Lebrun <125732067+netmico@users.noreply.github.com>
2024-09-08 08:33:22 -0400

    Update Vnet.tf

--- Vnet.tf
+++ Vnet.tf
@@ -28,9 +28,9 @@ resource "azurerm_network_security_rule" "inbound_rule" {
     priority                    = 100
     direction                   = "Inbound"
     access                      = "Allow"
-    protocol                    = "*"
+    protocol                    = "TCP"
     source_port_range           = "*"
-    destination_port_range      = "*"
+    destination_port_range      = "22"
     source_address_prefix       = "*"
     destination_address_prefix  = "*"
     resource_group_name         = azurerm_resource_group.az_resources.name
